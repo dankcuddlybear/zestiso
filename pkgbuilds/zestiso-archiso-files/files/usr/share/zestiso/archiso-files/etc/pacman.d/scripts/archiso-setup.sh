c327c5a3fda2145868f7ef30dc4b42bf22a77c01
Dankcuddlybear <dankcuddlybear@proton.me>
2025-01-24 14:45:48 +0000

    Update

--- pkgbuilds/zestiso-archiso-files/files/usr/share/zestiso/archiso-files/etc/pacman.d/scripts/archiso-setup.sh
+++ pkgbuilds/zestiso-archiso-files/files/usr/share/zestiso/archiso-files/etc/pacman.d/scripts/archiso-setup.sh
@@ -19,7 +19,7 @@ pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 
 ## Replace mkinitcpio.conf so that mkinitcpio is properly configured for Arch install
-mv /etc/mkinitcpio.conf.system /etc/mkinitcpio.conf
+cp -f /etc/mkinitcpio.conf.system /etc/mkinitcpio.conf
 
 ## Generate locales
 locale-gen
