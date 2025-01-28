9882bc238dc7f2d2e26700a4ed866746cb946b96
Dankcuddlybear <dankcuddlybear@proton.me>
2025-01-28 13:08:36 +0000

    sdv

--- pkgbuilds/zestiso-archiso-files/files/usr/share/zestiso/archiso-files/etc/pacman.d/scripts/archiso-setup.sh
+++ pkgbuilds/zestiso-archiso-files/files/usr/share/zestiso/archiso-files/etc/pacman.d/scripts/archiso-setup.sh
@@ -9,7 +9,6 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 ## Enable important system services
 systemctl enable fstrim.timer systemd-timesyncd
 
-
 ## Initialise Pacman keyring
 pacman-key --init
 pacman-key --populate
@@ -19,11 +18,9 @@ pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 
 ## Replace mkinitcpio.conf so that mkinitcpio is properly configured for Arch install
-cp -f /etc/mkinitcpio.conf.system /etc/mkinitcpio.conf
-
-## Generate locales
-locale-gen
+rm -f /etc/mkinitcpio.conf
+mv /etc/mkinitcpio.conf.system /etc/mkinitcpio.conf
 
 ## Delete hooks/scripts only used for ArchISO image generation
-sh -c "rm -- $(grep -Frl 'remove from airootfs' /etc/pacman.d/hooks/)"
+rm -f /etc/pacman.d/hooks/zzzz-archiso-setup.hook
 rm -f /etc/pacman.d/scripts/archiso-setup.sh
