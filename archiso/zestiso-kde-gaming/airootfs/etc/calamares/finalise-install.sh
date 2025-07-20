666e6cf321d6d7d15ec44dde8b7c8e12b2ad2b8f
Dankcuddlybear <dankcuddlybear@proton.me>
2025-07-20 23:04:58 +0200

    Stability improvements to optimise user experience

--- archiso/zestiso-kde-gaming/airootfs/etc/calamares/finalise-install.sh
+++ archiso/zestiso-kde-gaming/airootfs/etc/calamares/finalise-install.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
-UNINSTALL_PKG=""
-MarkPkgForRemoval() {
-	(pacman -Qi $1 &> /dev/null) && UNINSTALL_PKG="$1 $UNINSTALL_PKG"
-}
+## Automatically remove drivers for GPUs not present
+UNNEEDED_PKGS=""
+if (! (lspci | grep -i "VGA compatible controller: NVIDIA" &> /dev/null)); then
+    echo "No NVIDIA gpus found, drivers will be uninstalled."
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-free"
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-pro"
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-pro-340xx"
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-pro-390xx"
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-pro-470xx"
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-nvidia-pro-525xx"
+else echo "Found NVIDIA GPU(s)"; fi
+if (! (lspci | grep -i "VGA compatible controller: Intel" &> /dev/null)); then
+    echo "No Intel gpus found, drivers will be uninstalled."
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-intel-gpu"
+else echo "Found Intel GPU(s)"; fi
+if (! (lspci | grep -i "VGA compatible controller: Advanced Micro Devices" &> /dev/null)); then
+    echo "No AMD gpus found, drivers will be uninstalled."
+	UNNEEDED_PKGS="$UNNEEDED_PKGS bundle-amd-gpu"
+else echo "Found AMD GPU(s)"; fi
+
+## Automatically remove microcode for CPUs not present
+CPU_VENDOR_ID="$(lscpu | grep -i "Vendor ID:" | cut -d ':' -f 2 | xargs)"
+[ -z "$CPU_VENDOR_ID" ] && CPU_VENDOR_ID="GenericCPU"
+if [ "$CPU_VENDOR_ID" != "GenuineIntel" ]; then
+    echo "No Intel CPU found, microcode will be uninstalled."
+	UNNEEDED_PKGS="$UNNEEDED_PKGS intel-ucode"
+else echo "Found Intel CPU"; fi
+if [ "$CPU_VENDOR_ID" != "AuthenticAMD" ]; then
+    echo "No AMD CPU found, microcode will be uninstalled."
+	UNNEEDED_PKGS="$UNNEEDED_PKGS amd-ucode"
+else echo "Found AMD CPU"; fi
+
+## Check which unneeded packages are still present, and add them to a list to uninstall
+PKGS_TO_UNINSTALL=""
+echo "Checking installed packages..."
+for PKG in $UNNEEDED_PKGS; do
+    pacman -Qi $PKG &> /dev/null && PKGS_TO_UNINSTALL="$PKGS_TO_UNINSTALL $PKG"
+done
+echo "To remove: $PKGS_TO_UNINSTALL"
+
+## Uninstall unneeded packages
+pacman --noconfirm -Rus $PKGS_TO_UNINSTALL
+
+echo "Marking dependencies..."
+pacman --asdeps -D bash gnu-free-fonts iptables-nft lib32-sdl12-compat libglvnd mkinitcpio noto-fonts ntfs-3g qt6-multimedia-gstreamer pacman phonon-qt6-gstreamer-git polkit wireplumber
 
 ## Create mkinitcpio preset
 rm /etc/mkinitcpio.d/*.preset
@@ -17,72 +58,6 @@ echo "#default_config=\"/etc/mkinitcpio.conf\"" >> /etc/mkinitcpio.d/$KERNEL_NAM
 echo "default_image=\"/boot/initramfs-$KERNEL_NAME.img\"" >> /etc/mkinitcpio.d/$KERNEL_NAME.preset
 echo "#default_uki=\"/efi/EFI/Linux/arch-$KERNEL_NAME.efi\"" >> /etc/mkinitcpio.d/$KERNEL_NAME.preset
 echo "#default_options=\"--splash /usr/share/systemd/bootctl/splash-arch.bmp\"" >> /etc/mkinitcpio.d/$KERNEL_NAME.preset
-
-## Remove unnecessary services
-echo "Removing unnecessary packages..."
-if (! (lspci | grep -i "VMware" &> /dev/null)) && \
-(! (lsusb | grep -i "VMware" &> /dev/null)); then
-	systemctl --now disable vmtoolsd vmware-vmblock-fuse &> /dev/null
-	MarkPkgForRemoval open-vm-tools; MarkPkgForRemoval gtkmm3; MarkPkgForRemoval libxtst
-fi
-if (! (lspci | grep -i "Hyper-V" &> /dev/null)) && \
-(! (lsusb | grep -i "Hyper-V" &> /dev/null)); then
-	systemctl --now disable hv_fcopy_daemon hv_kvp_daemon hv_vss_daemon &> /dev/null
-	MarkPkgForRemoval hyperv
-fi
-if (! (lspci | grep -i "VirtualBox" &> /dev/null)) && \
-(! (lsusb | grep -i "VirtualBox" &> /dev/null)); then
-	systemctl --now disable vboxservice &> /dev/null
-	MarkPkgForRemoval virtualbox-guest-utils; MarkPkgForRemoval virtualbox-guest-utils-nox
-fi
-if (! (lspci | grep -i "Virtio" &> /dev/null)) && \
-(! (lspci | grep -i "QXL" &> /dev/null)) && \
-(! (lspci | grep -i "Cirrus" &> /dev/null)) && \
-(! (lsusb | grep -i "Virtio" &> /dev/null)); then
-	systemctl --now disable qemu-guest-agent &> /dev/null
-	MarkPkgForRemoval qemu-guest-agent
-fi
-if (! (lspci | grep -i "VGA compatible controller: NVIDIA" &> /dev/null)); then
-	MarkPkgForRemoval bundle-nvidia-free
-	MarkPkgForRemoval bundle-nvidia-pro
-	MarkPkgForRemoval bundle-nvidia-pro-340xx
-	MarkPkgForRemoval bundle-nvidia-pro-390xx
-	MarkPkgForRemoval bundle-nvidia-pro-470xx
-	MarkPkgForRemoval bundle-nvidia-pro-525xx
-fi
-if (! (lspci | grep -i "VGA compatible controller: Intel" &> /dev/null)); then
-	MarkPkgForRemoval bundle-intel-gpu
-fi
-if (! (lspci | grep -i "VGA compatible controller: Advanced Micro Devices" &> /dev/null)); then
-	MarkPkgForRemoval bundle-amd-gpu
-fi
-if [ "$(lscpu | grep -i "Vendor ID:" | cut -d ':' -f 2 | xargs)" != "GenuineIntel" ]; then
-	MarkPkgForRemoval intel-ucode
-fi
-if [ "$(lscpu | grep -i "Vendor ID(pacman -Qi $1 &> /dev/null) && UNINSTALL_PKG="$1 $UNINSTALL_PKG":" | cut -d ':' -f 2 | xargs)" != "AuthenticAMD" ]; then
-	MarkPkgForRemoval amd-ucode
-fi
-MarkPkgForRemoval arch-install-scripts
-MarkPkgForRemoval archinstall
-MarkPkgForRemoval boost1.86-libs
-MarkPkgForRemoval calamares
-MarkPkgForRemoval edk2-shell
-MarkPkgForRemoval memtest86+
-MarkPkgForRemoval memtest86+-efi
-MarkPkgForRemoval mkinitcpio-archiso
-MarkPkgForRemoval mkinitcpio-netconf
-MarkPkgForRemoval mkinitcpio-nfs-utils
-MarkPkgForRemoval pv
-MarkPkgForRemoval syslinux
-MarkPkgForRemoval zestiso-archiso-files
-MarkPkgForRemoval zestiso-archiso-files-dev
-MarkPkgForRemoval zestiso-branding
-
-echo "Uninstalling unnecessary packages..."
-pacman --noconfirm -Rus $UNINSTALL_PKG &> /dev/null
-
-echo "Marking dependencies..."
-pacman --asdeps -D bash gnu-free-fonts iptables-nft lib32-sdl12-compat libglvnd mkinitcpio noto-fonts ntfs-3g qt6-multimedia-gstreamer pacman phonon-qt6-gstreamer-git polkit wireplumber &> /dev/null
 
 ## Enable sudo for wheel members
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/10_wheel
